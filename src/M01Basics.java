0118c0489765492db2332b2c7cc022e0dc34d7aa
Maicon Lucinda <maiconlucinda@gmail.com>
2025-06-30 15:21:41 +0100

    historia Java

---
+++ src/M01Basics.java
@@ -0,0 +1,30 @@
+/* HISTORIA DO JAVA */
+/* A ideia inicial era ter uma solucao para dispositivos eletronicos intgligentes.
+* A Sun financiou uma pesquisa que teve o nome Green em 1991.
+* O desenvolvimento da linguagem foi baseada em C e C++, seu criador foi James Gasling. */
+
+
+/* MOTIVACOES */
+/* A ideia era criar um interpretador para pequenos dispositivos, facilitando a reescrita de software para aparelehos
+* eletronicos. */
+
+
+/* PRINCIPAIS CARACTERISTICAS */
+/* - Independente de plataforma: programadas Java são compilados para uma forma intermediária (bytecode)
+* - Orientada a objetos: Java é uma linguagem puramente orientada a objetos; tudo são classes ou instancias, com exceção dos tipos primitivos
+* - Coletor de Lixo (Garbage Collector): Possui um mecanismo automatico de gerenciamento de memória
+* - Permite Multithrearing: Recurso que permite o desenvovimento de aplicações que executam ações concorrentemente */
+
+
+/* COMPONENTES JAVA */
+/* JDK: Composto do compilador (javac + JVM) */
+/* JRE: Java Runtime Enviromment, é um JVM que permite a execução de softwares em Java */
+
+
+
+
+public class M01Basics {
+    public static void main(String[] args) {
+        System.out.println("My First Class Java..");
+    }
+}
