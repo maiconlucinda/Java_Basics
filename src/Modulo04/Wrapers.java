ea36b05cbb6b5a9eb4639344dfa01f1cddef9b0f
Maicon Lucinda <maiconlucinda@gmail.com>
2025-07-11 17:14:10 +0100

    Wrappers

---
+++ src/Modulo04/Wrapers.java
@@ -0,0 +1,36 @@
+package Modulo04;
+
+public class Wrapers {
+    public static void main(String[] args) {
+        System.out.println("Understanding Wrapers..");
+    }
+}
+
+
+
+// Wrappers
+/* Os tipos primitivos no Java são dados básicos, são simplesmente dados armazenados na memória
+* A ideia é que eles sejam rápidos e que possam consumir menos memória. Eles guardam o valor em si, não uma referencia */
+/* Então surge a necessidade de usar os Wrappers, estes conseguem fazer com que um dado básico (tipos primitivos)
+* funcionem como se fossem um objeto.
+* Os Wrappers são classes que representam os tipos primitivos. Eles são imutáveis, uma vez criado o valor não
+* pode ser mudado */
+
+// Integer - int
+// Long - long
+// Float - float
+// Double - double
+// Short - short
+// Byte - byte
+// Boolean - boolean
+// Character - char
+
+
+
+// Object
+/* Em java, todoo objeto é uma instancia que direta, ou indiretamente, herda da classe Object.
+ * A classe object é a superclasse raíz de todas as classes em Java
+ * Entendendo isso, podemos entender que até mesmo os Wrappers herdam da classe Object */
+
+
+
